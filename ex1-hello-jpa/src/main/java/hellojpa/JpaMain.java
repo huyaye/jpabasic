46ee879a8101bb9330f0c7627458e620bc725a9e
jungwookryu <huyaye@naver.com>
2019-12-03 16:39:50 +0900

    Primary Key mapping strategy

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -14,14 +14,34 @@ public class JpaMain {
 		EntityTransaction tx = em.getTransaction();
 		tx.begin();
 
-		Member member = new Member();
-		member.setId(1L);
-		member.setName("HelloA");
+		try {
+			Member member = new Member();
+			member.setUsername("A");
 
-		em.persist(member);
-		tx.commit();
+			Member member2 = new Member();
+			member2.setUsername("B");
 
-		em.close();
+			Member member3 = new Member();
+			member3.setUsername("C");
+
+			System.out.println("=============================");
+
+			em.persist(member);
+			em.persist(member2);
+			em.persist(member3);
+
+			System.out.println("memberId : " + member.getId());
+			System.out.println("member2Id : " + member2.getId());
+			System.out.println("member3Id : " + member3.getId());
+			System.out.println("=============================");
+
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+		} finally {
+			em.close();
+		}
+
 		emf.close();
 	}
 
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,14 +1,42 @@
 package hellojpa;
 
+import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
+@SequenceGenerator(name = "MEMBER_SEQ_GENERATOR", sequenceName = "MEMBER_SEQ", initialValue = 1, allocationSize = 50)
 public class Member {
 	@Id
+	//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
 	private Long id;
 
-	private String name;
+	@Column(name = "name")
+	private String username;
+
+	private Integer age;
+
+	@Enumerated(EnumType.STRING)
+	private RoleType roleType;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date lastModifiedDate;
+
+	@Lob
+	private String description;
 
 	public Long getId() {
 		return id;
@@ -18,12 +46,52 @@ public class Member {
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
+	public String getUsername() {
+		return username;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public RoleType getRoleType() {
+		return roleType;
+	}
+
+	public void setRoleType(RoleType roleType) {
+		this.roleType = roleType;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Date getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+
+	public void setLastModifiedDate(Date lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 }
