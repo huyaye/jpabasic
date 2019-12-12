d706ceceafe2efcabc767e1a1234693054667abc
jungwookryu <huyaye@naver.com>
2019-12-12 16:15:59 +0900

    Add member APIs - Add, Update, Query

---
+++ jpashop/src/main/java/jpabook/jpashop/api/dto/MemberAPIDto.java
@@ -0,0 +1,96 @@
+package jpabook.jpashop.api.dto;
+
+import javax.validation.constraints.NotEmpty;
+
+public class MemberAPIDto {
+	public static class Result<T> {
+		private T data;
+
+		public Result(T data) {
+			this.data = data;
+		}
+
+		public T getData() {
+			return data;
+		}
+	}
+
+	public static class CreateMemberRequest {
+		@NotEmpty
+		private String name;
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+	}
+
+	public static class CreateMemberResponse {
+		private Long id;
+
+		public CreateMemberResponse(Long id) {
+			this.id = id;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+	}
+
+	public static class UpdateMemberRequest {
+		private String name;
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+	}
+
+	public static class UpdateMemberResponse {
+		private Long id;
+		private String name;
+
+		public UpdateMemberResponse(Long id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+	}
+
+	public static class MemberResponse {
+		private String name;
+
+		public MemberResponse(String name) {
+			this.name = name;
+		}
+
+		public String getName() {
+			return name;
+		}
+	}
+}
--- jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -14,37 +14,47 @@ public class MemberService {
 	@Autowired
 	private MemberRepository memberRepository;
 
-//    @Autowired //테스트 케이스에 용의하다.
-//    public MemberService(MemberRepository memberRepository) {
-//        this.memberRepository = memberRepository;
-//    }
+	//    @Autowired //테스트 케이스에 용의하다.
+	//    public MemberService(MemberRepository memberRepository) {
+	//        this.memberRepository = memberRepository;
+	//    }
 
-    /**
-     * 회원 가입
-     */
-    @Transactional
-    public Long join(Member member){
-        validateDuplicateMember(member);
-        memberRepository.save(member);
-        return member.getId();
-    }
+	/**
+	 * 회원 가입
+	 */
+	@Transactional
+	public Long join(Member member) {
+		validateDuplicateMember(member);
+		memberRepository.save(member);
+		return member.getId();
+	}
 
-    private void validateDuplicateMember(Member member) {
-        List<Member> findMembers = memberRepository.findByName(member.getName());
-        if(!findMembers.isEmpty()){
-            throw new IllegalStateException("이미 존재하는 회원입니다.");
-        }
-    }
+	private void validateDuplicateMember(Member member) {
+		List<Member> findMembers = memberRepository.findByName(member.getName());
+		if (!findMembers.isEmpty()) {
+			throw new IllegalStateException("이미 존재하는 회원입니다.");
+		}
+	}
 
-    /**
-     * 회원 전체 조회
-     */
-    public List<Member> findMembers(){
-        return memberRepository.findAll();
-    }
+	/**
+	 * 회원 전체 조회
+	 */
+	public List<Member> findMembers() {
+		return memberRepository.findAll();
+	}
 
-    public Member findOne(Long memberId){
-        return memberRepository.findOne(memberId);
-    }
+	public Member findOne(Long memberId) {
+		return memberRepository.findOne(memberId);
+	}
+
+	/**
+	* 회원 수정
+	* JPA 변경 감지 사용
+	*/
+	@Transactional
+	public void update(Long id, String name) {
+		Member member = memberRepository.findOne(id);
+		member.setName(name);
+	}
 }
 
